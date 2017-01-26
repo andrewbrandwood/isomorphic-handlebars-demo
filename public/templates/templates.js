299d7ea6be46f5dafff424d35af3744327b12ffe
andrew@brandwood.com <andrew@brandwood.com>
2017-01-26 16:16:35 +0000

    styles: adding list styles

--- public/templates/templates.js
+++ public/templates/templates.js
@@ -3,9 +3,9 @@ this["Demo"]["templates"] = this["Demo"]["templates"] || {};
 this["Demo"]["templates"]["item-listing"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function";
 
-  return "<div>\n  <h2>"
+  return "<div class=\"item-listing\">\n  <h2 class=\"item-listing__title\">"
     + container.escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</h2>\n  <div>\n    "
+    + "</h2>\n  <div class=\"item-listing__description\">\n    "
     + ((stack1 = ((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper))) != null ? stack1 : "")
     + "\n  </div>\n"
     + ((stack1 = container.invokePartial(partials["button/button"],depth0,{"name":"button/button","hash":{"buttonText":(depth0 != null ? depth0.linkText : depth0)},"data":data,"indent":"  ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
