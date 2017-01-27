9b8466ae52281d3b44d94fc8ea813b2d4979bcb2
andrew@brandwood.com <andrew@brandwood.com>
2017-01-27 16:02:55 +0000

    chore: removing comments

--- controllers/home.js
+++ controllers/home.js
@@ -13,36 +13,20 @@ var HomeController = {
 		var colorFilter = request.query.color || 'all';
 		var baseUrl = request.protocol + '://' + request.headers.host + '/search';
 		var query = request.url;
-		console.log(baseUrl + query);
 
 		promiseGet(baseUrl + query).then(function (data) {
-			console.log(data);
 			response.render('index', {
 				layout: false,
 				helpers: templateHelpers,
 				contentData: JSON.parse(data)
 			});
 		}).catch(function (err) {
-			//console.log(request);
 			response.render('index', {
 			 layout: false,
 			 helpers: templateHelpers,
 			 contentData: 'error'
 		 });
 		});
-
-
-		// fs.stat(dataUrl, function(err) {
-		// 	var data = null;
-		// 	if (err === null) {
-		// 		data = require(dataUrl);
-		// 	}
-		// 	response.render('index', {
-		// 		layout: false,
-		// 		helpers: templateHelpers,
-		// 		contentData: data
-		// 	});
-		// });
 	}
 };
 
