17bb99f8ea2afd9f3b731cdcbebd2d0ffeca2a7b
andrew@brandwood.com <andrew@brandwood.com>
2017-02-09 13:40:39 +0000

    chore: adding some ES6

--- public/js/main.js
+++ public/js/main.js
@@ -2,81 +2,76 @@
 	/*jshint undef:false*/
 	'use strict';
 
-	var Website = Website || {};
+	function isCorrectButton(e, button){
+		return Object.getOwnPropertyDescriptor(e.target.dataset, button) === undefined ? false : true;
+	}
 
-	Website = function() {
+	function getData(e){
+		if (!isCorrectButton(e, 'demoButton')) return;
 
-		var _self = this;
+		const filter = e.target.value;
 
-		function getData(e){
-			var myButton = Object.getOwnPropertyDescriptor(e.target.dataset, 'demoButton');
-			if(myButton === undefined) return;
+		const request = new XMLHttpRequest();
+		request.open('GET', '/search/?color=' + filter, true);
 
-			var filter = e.target.value;
+		request.onload = () => {
+		  if (request.status >= 200 && request.status < 400) {
+		    // Success!
+		    const data = JSON.parse(request.responseText);
+				addPartial(data);
+		  } else {
+		    // We reached our target server, but it returned an error
+				handleError('error');
+		  }
+		};
 
-			var request = new XMLHttpRequest();
-			request.open('GET', '/search/?color=' + filter, true);
+		request.onerror = () => {
+		  // There was a connection error of some sort
+			handleError('connection error');
+		};
 
-			request.onload = function() {
-			  if (request.status >= 200 && request.status < 400) {
-			    // Success!
-			    var data = JSON.parse(request.responseText);
-					addPartial(data);
-			  } else {
-			    // We reached our target server, but it returned an error
-					handleError('error');
-			  }
-			};
+		request.send();
 
-			request.onerror = function() {
-			  // There was a connection error of some sort
-				handleError('connection error');
-			};
+	}
 
-			request.send();
+	function handleError(error){
+		console.log(error);
+	}
 
-		}
+	function addPartial(data){
+		/* Non nested partial */
+		var template = window.Demo.templates['item-list'];
+		var container = document.querySelector('[data-item-list]');
+		container.innerHTML = '';
+		container.innerHTML += template({contentData:data});
+	}
 
-		function handleError(error){
-			console.log(error);
-		}
+	function registerPartials(){
+		/* Register the partial here as it will we re-used throughout.
+			This is a nested partial within a partial.
+			If a non nested partial then a template model is used.
+		*/
+		Handlebars.registerPartial('item-listing/item-listing', window.Demo.templates['item-listing']);
+		Handlebars.registerPartial('button/button', window.Demo.templates['button']);
+	}
 
-		function addPartial(data){
-			/* Non nested partial */
-			var template = window.Demo.templates['item-list'];
-			var container = document.querySelector('[data-item-list]');
-			container.innerHTML = '';
-			container.innerHTML += template({contentData:data});
-		}
+	function addEventListeners(){
+		document.addEventListener('click', getData);
+	}
 
-		function registerPartials(){
-			/* Register the partial here as it will we re-used throughout.
-				This is a nested partial within a partial.
-				If a non nested partial then a template model is used.
-			*/
-			Handlebars.registerPartial('item-listing/item-listing', window.Demo.templates['item-listing']);
-			Handlebars.registerPartial('button/button', window.Demo.templates['button']);
-		}
+	function addJSHelper(){
+		// adds a class to the html element so we can hide element that are not needed when JS is available.
+		document.getElementsByTagName('html')[0].classList.add('js');
+	}
 
-		function addEventListeners(){
-			document.addEventListener('click', getData);
-		}
+	function init() {
+		registerPartials();
+		addEventListeners();
+		addJSHelper();
+	}
 
-		function addJSHelper(){
-			// adds a class to the html element so we can hide element that are not needed when JS is available.
-			document.getElementsByTagName('html')[0].classList.add('js');
-		}
+	init();
 
-		function init() {
-			registerPartials();
-			addEventListeners();
-			addJSHelper();
-		}
-
-		init();
-	};
-
-	var website = new Website();
 
 })(window);
 
