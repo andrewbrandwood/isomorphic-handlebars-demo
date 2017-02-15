d356517c147ffddea5dd5b32a62602c36e4e7f1f
andrew@brandwood.com <andrew@brandwood.com>
2017-02-15 12:38:25 +0000

    chore: removing code for task

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,10 @@
 (function(window, undefined) {
 	'use strict';
 
-	function isCorrectButton(e, button){
-		return Object.getOwnPropertyDescriptor(e.target.dataset, button) === undefined ? false : true;
-	}
-
-	function getData(e){
-		if (!isCorrectButton(e, 'demoButton')) return;
-
-		const filter = e.target.value;
-
-		const request = new XMLHttpRequest();
-		request.open('GET', `/search/?color=${filter}`, true);
-
-		request.onload = () => {
-		  if (request.status >= 200 && request.status < 400) {
-		    // Success!
-		    const data = JSON.parse(request.responseText);
-				addPartial(data);
-		  } else {
-		    // We reached our target server, but it returned an error
-				handleError('error');
-		  }
-		};
-
-		request.onerror = () => {
-		  // There was a connection error of some sort
-			handleError('connection error');
-		};
-		request.send();
-	}
-
-	function handleError(error){
-		console.log(error);
-	}
-
-	function addPartial(data){
-		/* Non nested partial */
-		var template = window.Demo.templates['item-list'];
-		var container = document.querySelector('[data-item-list]');
-		container.innerHTML = template({contentData:data});
-	}
-
-	function registerPartials(){
-		/* Register the partial here as it will we re-used throughout.
-			This is a nested partial within a partial.
-			If a non nested partial then a template model is used.
-		*/
-		Handlebars.registerPartial('item-listing/item-listing', window.Demo.templates['item-listing']);
-		Handlebars.registerPartial('button/button', window.Demo.templates['button']);
-	}
-
-	function addEventListeners(){
-		document.addEventListener('click', getData);
-	}
-
-	function addJSHelper(){
-		// adds a class to the html element so we can hide element that are not needed when JS is available.
-		document.getElementsByTagName('html')[0].classList.add('js');
-	}
-
 	function init() {
-		registerPartials();
-		addEventListeners();
-		addJSHelper();
+		console.log('ready to start');
 	}
 
 	init();
-
 
 })(window);
